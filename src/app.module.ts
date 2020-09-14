054d49a2fdeb68051d13cfc886eba06f419d743f
Ryan Craig Martin <ryan@ryanmartin.io>
2020-09-14 16:08:15 -0400

    add: tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [], //? List of modules required by this module.
+  imports: [TasksModule], //? List of modules required by this module.
   exports: [], //? List of providers to export to other modules.
   controllers: [AppController], //? List of controllers to be instantiated within this module.
   providers: [AppService], //? List of providers to be available within this module via dep-injection.
