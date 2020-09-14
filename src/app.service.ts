410beb6bea9922e2948374114f90b60da9688362
Ryan Craig Martin <ryan@ryanmartin.io>
2020-09-14 19:57:22 -0400

    update: app index

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  serverStatus(): Record<string, unknown> {
+    return { status: 'ok' }
   }
 }
