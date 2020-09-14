f8a372bf030b29a08d594943f7ea5f734492c5bc
Ryan Craig Martin <ryan@ryanmartin.io>
2020-09-14 16:21:25 -0400

    add: tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,9 @@
+import { TasksService } from './tasks.service'
 import { Controller } from '@nestjs/common'
 
-@Controller('tasks') // Any routes dealing with tasks will be handled by this controller
-export class TasksController {}
+@Controller('tasks') //? Any routes dealing with tasks will be handled by this controller
+export class TasksController {
+  constructor(private tasksService: TasksService) {
+    //? Allows any method to have access to our service
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable() // Makes it available for injection in other modules.
+export class TasksService {}
